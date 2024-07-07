58f2f2c28921cfbe9e3ee8fe960a7644c7b70008
DarkhanDaurenov <takishevdarkhan@gmail.com>
2024-07-07 16:01:37 +0500

    add users/views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import filters
-from rest_framework.generics import ListAPIView
-from users.models import Payment
-from users.serializers import PaymentSerializer
+from rest_framework.generics import ListAPIView, CreateAPIView
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UsersSerializer
+from rest_framework.permissions import AllowAny
+
 
 class PaymentListAPIView(ListAPIView):
     queryset = Payment.objects.all()
@@ -9,5 +11,18 @@ class PaymentListAPIView(ListAPIView):
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['payment_date']
     search_fields = ['paid_course__title', 'paid_lesson__title', 'payment_method']
+
+
+class UsersCreateAPIView(CreateAPIView):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
 
 
