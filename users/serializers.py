42cc1980248ce9cb6fa645891e968c67485ffeff
DarkhanDaurenov <takishevdarkhan@gmail.com>
2024-07-20 14:06:28 +0500

    add users/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from users.models import Payment, User
 
-class PaymentSerializer(ModelSerializer):
+
+class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = "__all__"
+        fields = ['id', 'paid_course', 'paid_lesson', 'payment_amount', 'payment_method', 'payment_date', 'link']
+        read_only_fields = ['user', 'payment_date', 'link']
 
 
-class UsersSerializer(ModelSerializer):
+class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['id', 'email', 'password', 'phone', 'city', 'avatar']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
